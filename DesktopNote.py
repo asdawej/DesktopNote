a5357ec00fa631233d7a0ede7ed4fafa085e6165
964293341 <asdawej123@163.com>
2024-05-22 20:12:02 +0800

    ADD workflows

--- DesktopNote.py
+++ DesktopNote.py
@@ -15,77 +15,71 @@ class TextRevise(cmd.Cmd):
         for _i, _x in enumerate(TEXT):
             if _i == self.y:
                 if TEXT[self.y] == '':
-                    print(str(_i)+'>', '><')
+                    print(str(_i) + '>', '><')
                 else:
                     print(
-                        str(_i)+'>',
+                        str(_i) + '>',
                         (
                             TEXT[self.y][:self.x]
-                            + '>'+TEXT[self.y][self.x]+'<'
-                            + TEXT[self.y][self.x+1:]
+                            + '>' + TEXT[self.y][self.x] + '<'
+                            + TEXT[self.y][self.x + 1:]
                         )
                     )
             else:
-                print(str(_i)+'>', _x)
+                print(str(_i) + '>', _x)
 
     def do_mv(self, arg):
-        '''Move to a location\
-        \n=====\
-        \nmv <row_order :: int> <char_order :: int>'''
+        '''Move to a location
+        mv <row_order :: int> <char_order :: int>'''
         arg = [int(x) for x in arg.split()]
         self.x = arg[1]
         self.y = arg[0]
         self.screen()
 
     def do_r(self, arg):
-        '''Right move\
-        \n=====\
-        \nr <length :: int>'''
+        '''Right move
+        r <length :: int>'''
         self.x += int(arg)
         self.screen()
 
     def do_l(self, arg):
-        '''Left move\
-        \n=====\
-        \nl <length :: int>'''
+        '''Left move
+        l <length :: int>'''
         self.x -= int(arg)
         self.screen()
 
     def do_u(self, arg):
-        '''Up move\
-        \n=====\
-        \nu <length :: int>'''
+        '''Up move
+        u <length :: int>'''
         self.y -= int(arg)
         self.screen()
 
     def do_d(self, arg):
-        '''Down move\
-        \n=====\
-        \nd <length :: int>'''
+        '''Down move
+        d <length :: int>'''
         self.y += int(arg)
         self.screen()
 
     def do_rm(self, arg):
-        '''Delete a char or chars\
-        \n=====\
-        \nrm [[<row_order :: int> <char_order :: int> [<length :: int>]] | [<length>]]'''
+        '''Delete a char or chars
+        rm [[<row_order :: int> <char_order :: int> [<length :: int>]] | [<length>]]'''
         arg = [int(x) for x in arg.split()]
         _leng = len(arg)
         # Delete string
         if _leng == 0:
-            TEXT[self.y] = TEXT[self.y][:self.x]+TEXT[self.y][self.x+1:]
+            TEXT[self.y] = TEXT[self.y][:self.x] + TEXT[self.y][self.x + 1:]
         elif _leng == 1:
             _l = arg[0]
-            TEXT[self.y] = TEXT[self.y][:self.x]+TEXT[self.y][self.x+_l:]
+            TEXT[self.y] = TEXT[self.y][:self.x] + TEXT[self.y][self.x + _l:]
         elif _leng == 2:
             _x = arg[1]
             _y = arg[0]
-            TEXT[_y] = TEXT[_y][:_x]+TEXT[_y][_x+1:]
+            TEXT[_y] = TEXT[_y][:_x] + TEXT[_y][_x + 1:]
         elif _leng == 3:
             _x = arg[1]
             _y = arg[0]
             _l = arg[2]
-            TEXT[_y] = TEXT[_y][:_x]+TEXT[_y][_x+_l:]
+            TEXT[_y] = TEXT[_y][:_x] + TEXT[_y][_x + _l:]
         # Move pointer
         if _leng == 2 or _leng == 3:
             self.x = _x
@@ -95,23 +89,22 @@ class TextRevise(cmd.Cmd):
             if _leng_s == 0:
                 self.x = 0
             else:
-                self.x = _leng_s-1
+                self.x = _leng_s - 1
         self.screen()
 
     def do_ad(self, arg):
-        '''Add a string on the right side\
-        \n=====\
-        \nad [<row_order :: int> <char_order :: int>]'''
+        '''Add a string on the right side
+        ad [<row_order :: int> <char_order :: int>]'''
         arg = [int(x) for x in arg.split()]
         _leng = len(arg)
         # Add string
         _s = input('Added string: ')
         if _leng == 0:
-            TEXT[self.y] = TEXT[self.y][:self.x+1]+_s+TEXT[self.y][self.x+1:]
+            TEXT[self.y] = TEXT[self.y][:self.x + 1] + _s + TEXT[self.y][self.x + 1:]
         elif _leng == 2:
             _x = arg[1]
             _y = arg[0]
-            TEXT[_y] = TEXT[_y][:_x+1]+_s+TEXT[_y][_x+1:]
+            TEXT[_y] = TEXT[_y][:_x + 1] + _s + TEXT[_y][_x + 1:]
         # Move pointer
         _leng_s = len(_s)
         if _leng == 2:
@@ -121,27 +114,26 @@ class TextRevise(cmd.Cmd):
         self.screen()
 
     def do_rv(self, arg):  # (x, y)或无, 将一个字符修改; (x, y, l)或l, 将 l 个字符修改
-        '''Revise a char or chars\
-        \n=====\
-        \nrv [[<row_order :: int> <char_order :: int> [<length :: int>]] | [<length>]]'''
+        '''Revise a char or chars
+        rv [[<row_order :: int> <char_order :: int> [<length :: int>]] | [<length>]]'''
         arg = [int(x) for x in arg.split()]
         _leng = len(arg)
         # Revise string
         _s = input('New string: ')
         if _leng == 0:
-            TEXT[self.y] = TEXT[self.y][:self.x]+_s+TEXT[self.y][self.x+1:]
+            TEXT[self.y] = TEXT[self.y][:self.x] + _s + TEXT[self.y][self.x + 1:]
         elif _leng == 1:
             _l = arg[0]
-            TEXT[self.y] = TEXT[self.y][:self.x]+_s+TEXT[self.y][self.x+_l:]
+            TEXT[self.y] = TEXT[self.y][:self.x] + _s + TEXT[self.y][self.x + _l:]
         elif _leng == 2:
             _x = arg[1]
             _y = arg[0]
-            TEXT[_y] = TEXT[_y][:_x]+_s+TEXT[_y][_x+1:]
+            TEXT[_y] = TEXT[_y][:_x] + _s + TEXT[_y][_x + 1:]
         elif _leng == 3:
             _x = arg[1]
             _y = arg[0]
             _l = arg[2]
-            TEXT[_y] = TEXT[_y][:_x]+_s+TEXT[_y][_x+_l:]
+            TEXT[_y] = TEXT[_y][:_x] + _s + TEXT[_y][_x + _l:]
         # Move pointer
         _leng_s = len(_s)
         if _leng == 2 or _leng == 3:
@@ -152,15 +144,14 @@ class TextRevise(cmd.Cmd):
             if _leng_ss == 0:
                 self.x = 0
             else:
-                self.x = _leng_ss-1
+                self.x = _leng_ss - 1
         else:
-            self.x += _leng_s-1
+            self.x += _leng_s - 1
         self.screen()
 
     def do_ex(self, arg):
-        '''Exit revising shell\
-        \n=====\
-        \nex'''
+        '''Exit revising shell
+        ex'''
         return True
 
 
@@ -172,7 +163,7 @@ class Note(cmd.Cmd):
     prompt = '<note>'
     try:
         fnote = open('note.dat', 'rb')
-    except:
+    except BaseException:
         fnote = open('note.dat', 'wb')
         pickle.dump([], fnote)
         fnote.close()
@@ -181,9 +172,8 @@ class Note(cmd.Cmd):
     fnote.close()
 
     def do_show(self, arg):
-        '''Show the notes\
-        \n=====\
-        \nshow [<note_order :: int>]'''
+        '''Show the notes
+        show [<note_order :: int>]'''
         os.system('cls')
         print('    ==========================================================')
         try:
@@ -191,20 +181,19 @@ class Note(cmd.Cmd):
             print('Title:', arg, self.note[arg][0])
             print('Text:')
             for i, s in enumerate(self.note[arg][1]):
-                print(str(i)+'>', s)
+                print(str(i) + '>', s)
             print('    ==========================================================')
-        except:
+        except BaseException:
             for i, x in enumerate(self.note):
                 print('Title:', i, x[0])
                 print('Text:')
                 for j, s in enumerate(x[1]):
-                    print(str(j)+'>', s)
+                    print(str(j) + '>', s)
                 print('    ==========================================================')
 
     def do_write(self, arg):
-        '''Write a new note\
-        \n=====\
-        \nwrite'''
+        '''Write a new note
+        write'''
         temp_title = input('New note title: ')
         temp_text: list[str] = []
         print('New note text(end with END):')
@@ -216,9 +205,8 @@ class Note(cmd.Cmd):
         self.fnote.close()
 
     def do_revise(self, arg):
-        '''Revise a note\
-        \n=====\
-        \nrevise <note_order :: int>'''
+        '''Revise a note
+        revise <note_order :: int>'''
         arg = int(arg)
         global TEXT
         TEXT = self.note[arg][1]
@@ -227,8 +215,8 @@ class Note(cmd.Cmd):
                 if x == '':
                     print('0>', '><')
                 else:
-                    print('0>', '>'+x[0]+'<'+x[1:])
-            print(str(i)+'>', x)
+                    print('0>', '>' + x[0] + '<' + x[1:])
+            print(str(i) + '>', x)
         TextRevise().cmdloop()
         self.note[arg][1] = TEXT
         self.fnote = open('note.dat', 'wb')
@@ -236,9 +224,8 @@ class Note(cmd.Cmd):
         self.fnote.close()
 
     def do_delete(self, arg):
-        '''Delete a note\
-        \n=====\
-        \ndelete <note_order :: int>'''
+        '''Delete a note
+        delete <note_order :: int>'''
         arg = int(arg)
         self.note.pop(arg)
         self.fnote = open('note.dat', 'wb')
@@ -246,10 +233,9 @@ class Note(cmd.Cmd):
         self.fnote.close()
 
     def do_exit(self, arg):
-        '''Exit DesktopNote\
-        \n=====\
-        \nexit'''
-        exit(0)
+        '''Exit DesktopNote
+        exit'''
+        return exit()
 
     def postcmd(self, stop: bool, line: str) -> bool:
         return False
